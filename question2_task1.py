8e3f5c036f7d20eafc8c42f588a4f785c0722ccc
cdsapkota <cdsapkota@gmail.com>
2024-09-11 19:31:03 -0500

    commiting all files.

---
+++ question2_task1.py
@@ -0,0 +1,7 @@
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+if "Alice" in submitted and "Alice" in attended:
+    print("Alice has submitted the assignment and attended the last class.")
+else:
+    print("Alice has not fullfilled her criteria.")
